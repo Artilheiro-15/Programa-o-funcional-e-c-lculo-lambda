8a01faac4da5d2d6fa2ee2c3360edc0cee4cd136
Atila jr <atilla.junior33@gmail.com>
2023-01-10 11:03:19 -0300

    Expreçao lambda = funçao anonima de primeira class

--- src/App.java
+++ src/App.java
@@ -1,23 +1,19 @@
-import java.util.Arrays;
+import entities.Product;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
 
-  public static int globalValue = 3;
-
-  public static void main(String[] args) throws Exception {
-    System.out.println("=======================================");
-
-    int[] vect = new int[] { 3, 4, 5 };
-    changeOddValues(vect);
-    System.out.println(Arrays.toString(vect));
-    System.out.println("=======================================");
+  public static int compareProducts(Product p1, Product p2) {
+    return p1.getPrice().compareTo(p2.getPrice());
   }
 
-  public static void changeOddValues(int[] numbers) {
-    for (int i = 0; i < numbers.length; i++) {
-      if (numbers[i] % 2 != 0) {
-        numbers[i] += globalValue;
-      }
-    }
+  public static void main(String[] args) {
+    List<Product> list = new ArrayList<>();
+    list.add(new Product("TV", 900.00));
+    list.add(new Product("Notebook", 1200.00));
+    list.add(new Product("Tablet", 450.00));
+    // list.sort(Program::compareProducts);
+    list.forEach(System.out::println);
   }
 }
